e96e963e6f8aa705952294bf65749f6233789f5d
Skrinz <111281257+Skrinz@users.noreply.github.com>
2024-12-14 23:12:28 +0800

    Removed redundant folder from repos

--- @web_dev_ni_cabas--/src/src/components/Modals/index.js
+++
@@ -1,3 +0,0 @@
-export { default as SettingsModal } from './SettingsModal';
-export { default as ProfileModal } from './ProfileModal';
-export { default as EditProfileModal } from './EditProfileModal'; 
